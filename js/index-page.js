5e7e074014b7b992fdcbfcff702cafffdbcd5831
beforyou <50078025+beforyou@users.noreply.github.com>
2026-05-02 19:25:15 +0800

    Update index-page.js

--- js/index-page.js
+++ js/index-page.js
@@ -45,7 +45,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 try {
                     window.history.replaceState(
                         { search: keyword }, 
-                        `搜索: ${keyword} - LibreTV`, 
+                        `搜索: ${keyword} - L TV`, 
                         window.location.href
                     );
                 } catch (e) {
