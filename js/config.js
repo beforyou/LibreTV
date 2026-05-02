9fe5151e3a60ca445c551564e343616ad3fb5de5
beforyou <50078025+beforyou@users.noreply.github.com>
2026-05-02 19:11:43 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -22,11 +22,12 @@ const SITE_CONFIG = {
 
 // API站点配置
 const API_SITES = {
-    testSource: {
+/*    testSource: {
         api: 'https://www.example.com/api.php/provide/vod',
         name: '空内容测试源',
         adult: true
     }
+    */
     //ARCHIVE https://telegra.ph/APIs-08-12
 };
 
